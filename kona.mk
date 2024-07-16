9c061f26ba06bdda10108b80337ae47256bba0a2
Flicker372 <flicker372@outlook.com>
2024-07-16 22:25:37 +0800

    sm8250-common: Add Support Full Fingerprint Payment
    [Ref]:
    1.https://github.com/AOSPA/android_device_xiaomi_psyche/commit/46030ea5ad817a70c406f2e0a735ea678e00cbba
    2.https://github.com/AOSPA/android_device_xiaomi_sepolicy/commit/e7ab73a8b64a032909bd140fae57e9db7592ef20
    3.https://github.com/AOSPA/android_device_xiaomi_sepolicy/commit/b60cac048b32cee0212ae2f71cc96671ec178a0b
    4.https://github.com/AOSPA/android_hardware_xiaomi/blob/uvite/vintf/xiaomi_framework_compatibility_matrix.xml
    
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- kona.mk
+++ kona.mk
@@ -321,13 +321,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Mlipay
-ifneq ($(TARGET_IS_TABLET),true)
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.2.vendor
-endif
-
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
