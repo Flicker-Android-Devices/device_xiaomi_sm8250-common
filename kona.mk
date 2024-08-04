243823e890787507cd1150095850cba37ae097da
Flicker372 <flicker372@outlook.com>
2024-08-04 20:05:30 +0800

    sm8250-common: overlay: Modify the default captive portal server for Chinese users
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- kona.mk
+++ kona.mk
@@ -423,6 +423,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CarrierConfigCommonOverlay \
     FrameworksCommonOverlay \
+    NetworkStackOverlayCN \
     SettingsCommonOverlay \
     SettingsProviderCommonOverlay \
     SystemUICommonOverlay \
